c9ab4c42ca9bdebcd7e805fce908cf26ecddbbe8
iamdannie <iamtaiyco@gmail.com>
2021-02-23 18:26:25 -0600

    TODO #2 view jobs by category  added link so that all link work-->

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/ListController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/ListController.java
@@ -32,6 +32,8 @@ public class ListController {
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
         tableChoices.put("coreCompetency", JobData.getAllCoreCompetency());
+//        tableChoices.put("All", JobData.findAll());
+
     }
 
     @RequestMapping(value = "")
@@ -42,6 +44,7 @@ public class ListController {
         model.addAttribute("locations", JobData.getAllLocations());
         model.addAttribute("positions", JobData.getAllPositionTypes());
         model.addAttribute("skills", JobData.getAllCoreCompetency());
+//        model.addAtrribute("A")
 
         return "list";
     }
