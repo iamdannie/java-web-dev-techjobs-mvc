0e4ddcd2a4d68593cd7218e03d00d72c92b51afd
iamdannie <iamtaiyco@gmail.com>
2021-02-23 20:36:00 -0600

    TODO #4 view jobs by category copy and paste previous loop to complete

--- src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/javawebdevtechjobsmvc/controllers/SearchController.java
@@ -1,8 +1,12 @@
 package org.launchcode.javawebdevtechjobsmvc.controllers;
 
+import org.launchcode.javawebdevtechjobsmvc.TechjobsVC.Job;
+import org.launchcode.javawebdevtechjobsmvc.TechjobsVC.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 import static org.launchcode.javawebdevtechjobsmvc.controllers.ListController.columnChoices;
 
@@ -20,5 +24,19 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
+        @PostMapping(value = "results")
+    public String DisplaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        ArrayList<Job> jobs= new ArrayList<>();
+        if (searchTerm.equals("all") || searchTerm.equals("")) {
+            jobs= JobData.findAll();
+        } else {
+           jobs= JobData.findByColumnAndValue(searchTerm, searchType);
+
+        }
+        model.addAttribute("jobs", jobs);
+        model.addAttribute("columns", columnChoices);
+        return "search";
+
+    }
 
 }
